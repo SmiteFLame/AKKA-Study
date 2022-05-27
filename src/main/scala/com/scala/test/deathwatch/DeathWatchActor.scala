3e815328613fb6731dca20984f1a1451905ab292
SmiteFLame <lukigreen@naver.com>
2022-05-27 20:07:02 +0900

    death watch

---
+++ src/main/scala/com/scala/test/deathwatch/DeathWatchActor.scala
@@ -0,0 +1,15 @@
+package com.scala.test.deathwatch
+
+import akka.actor.{Actor, Props, Terminated}
+
+class DeathWatchActor extends Actor{
+  val child = context.actorOf(Props[ServiceActor], "serviceActor")
+  context.watch(child)
+
+  override def receive: Receive = {
+    case Service => child ! Service
+    case Kill =>
+      context.stop(child)
+    case Terminated(`child`) => println("The service actor has terminated and no longer available")
+  }
+}
---
+++ src/main/scala/com/scala/test/deathwatch/ServiceActor.scala
@@ -0,0 +1,12 @@
+package com.scala.test.deathwatch
+
+import akka.actor.Actor
+
+case object Service
+case object Kill
+
+class ServiceActor extends Actor{
+  override def receive: Receive = {
+    case Service => println("I provide a special service")
+  }
+}
